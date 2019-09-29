6f0efb6076e8c896a2919a11c31ab6c56ec960ab
Chris <chris@builtbypixel.com>
2019-09-29 21:08:54 +0100

    test for the email

---
+++ app/Http/SearchFilters/Sandbox/Filters/Email_Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\SearchFilters\Sandbox\Filters;
+
+use App\SearchFilters\Filter;
+
+class Email_Test implements Filter
+{
+    public static function apply($builder, $value)
+    {
+        return $builder->where(strtolower((new \ReflectionClass(get_called_class()))->getShortName()), 'like', '%' . $value . '%');
+    }
+}
