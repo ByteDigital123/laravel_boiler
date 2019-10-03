f994ecf58ccd40bd5267d7e39a4f0431dc2adb39
Chris <chris@builtbypixel.com>
2019-10-03 13:53:42 +0100

    update to the seeder

--- database/factories/SandboxFactory.php
+++ database/factories/SandboxFactory.php
@@ -7,10 +7,11 @@ use App\AdminUser;
 use Faker\Generator as Faker;
 
 $factory->define(Sandbox::class, function (Faker $faker) {
+    $origins = ['Web Orders', 'Phone Orders', 'Shop Orders', 'Sagepay Orders'];
     return [
         'admin_user_id' => factory(AdminUser::class),
-        'name' => $faker->name,
-        'origin' => 'website',
+        'name' => $faker->company,
+        'origin' => $origins[array_rand($origins, 1)],
         'voucher' => rand(0, 1)
     ];
 });
